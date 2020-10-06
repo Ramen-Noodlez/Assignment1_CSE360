78de03549c0cddc2d00784845cf21628e2fc309d
dmrat <dmrat@DESKTOP-IIEF9CO>
2020-10-05 20:39:23 -0700

    Version 1 of CSE360 Assignment2 >Added Javadoc style comments to each method and class provided.

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assignment02;
+
+/*
+ * allows an integer perform add and subtract operations while tracking history of each operation.
+ */
+public class AddingMachine {
+  private int total;
+  
+  /*
+   * constructor to instantiate total as 0
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /*
+   * get the current total value
+   * @return current total value 
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /*
+   * add a value to the total
+   * @param value to be added to total
+   */
+  public void add (int value) {
+  }
+
+  /*
+   * subtract a value to the total
+   * @param value to be subtracted to total
+   */
+  public void subtract (int value) {
+  }
+
+  /*
+   * create a string representing the history of every add/subtract made to total
+   * @return string representing history of the program
+   */
+  public String toString () {
+    return "";
+  }
+
+  /*
+   * clear the memory of the program
+   */
+  public void clear() {
+  }
+}
